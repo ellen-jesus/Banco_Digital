17eee8735615e48beaa885f410dcc6bc8700c4ce
ellen mendes rosa torres de jesus <ellen20.torres@gmail.com>
2022-03-24 21:36:36 -0300

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,15 @@
+module com.example.banco_digital {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.banco_digital to javafx.fxml;
+    exports com.example.banco_digital;
+}
